c33b890aef4af632ee4edb03cad06948d369e35b
Dimokus <Dimokus@mail.ru>
2022-04-21 19:41:12 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,22 +4,21 @@ echo 'export POOL='${POOL} >> $HOME/.bashrc
 echo 'export MNEMONIC='${MNEMONIC} >> $HOME/.bashrc
 echo 'export STAKE='${STAKE} >> $HOME/.bashrc
 echo 'export MONIKER='${MONIKER} >> $HOME/.bashrc
+echo 'export LINK_ARWEAVE_WALLET='${LINK_ARWEAVE_WALLET} >> $HOME/.bashrc
+echo 'export LINK_KYVE_VERSION='${LINK_KYVE_VERSION} >> $HOME/.bashrc
 source $HOME/.bashrc
 
-wget "https://drive.google.com/uc?export=download&id=16HOfjRTzG53WYH6BKVj-OZwsE9sKrZgz"
 
-mv 'uc?export=download&id=16HOfjRTzG53WYH6BKVj-OZwsE9sKrZgz' /root/arweave.json
+wget -O arweave.json ${LINK_ARWEAVE_WALLET} 
+mv arweave.json /root/arweave.json
 
 cd $HOME && \
-wget https://github.com/KYVENetwork/evm/releases/download/v1.0.2/evm-linux.zip && \
+wget ${LINK_KYVE_VERSION} && \
 unzip evm-linux.zip && \
 rm evm-linux.zip && \
 mv evm-linux /usr/bin/kyve
 
 kyve --version
 
-echo "${MNEMONIC}"
-
-$(which kyve) --name "${MONIKER}" --poolId ${POOL} --mnemonic "${MNEMONIC}" --initialStake ${STAKE} --keyfile /root/arweave.json --network korellia --verbose
-
-
+$(which kyve) --name "${MONIKER}" --poolId ${POOL} --mnemonic "${MNEMONIC}" -s "${STAKE}" --keyfile /root/arweave.json --network korellia --verbose
+sleep infinity
