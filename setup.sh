48f57c10fcd660a1c30e9a41f3aa33ef94cf5488
Dimokus <Dimokus@mail.ru>
2022-04-26 23:08:24 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo 'export POOL='${POOL} >> $HOME/.bashrc
 echo 'export MNEMONIC='${MNEMONIC} >> $HOME/.bashrc
 echo 'export STAKE='${STAKE} >> $HOME/.bashrc
 echo 'export MONIKER='${MONIKER} >> $HOME/.bashrc
 echo 'export LINK_ARWEAVE_WALLET='${LINK_ARWEAVE_WALLET} >> $HOME/.bashrc
 echo 'export LINK_KYVE_VERSION='${LINK_KYVE_VERSION} >> $HOME/.bashrc
+POOL=0
 source $HOME/.bashrc
 
 
@@ -23,5 +23,20 @@ kyve --version
 for ((;;))
 do
 $(which kyve) --name "${MONIKER}" --poolId ${POOL} --mnemonic "${MNEMONIC}" -s "${STAKE}" --keyfile /root/arweave.json --network korellia --verbose
-sleep 10m
+sleep 5m
+if [[ ${POOL} -gt 0 ]]
+then
+	POOL=0
+	echo ======================================
+	echo ="ВНИМАНИЕ, СМЕНА ПУЛА! Выбран пул 0"=
+	echo ======================================
+else 
+	POOL=1
+	echo ======================================
+	echo ="ВНИМАНИЕ, СМЕНА ПУЛА! Выбран пул 1"=
+	echo ======================================
+fi
 done
+
+
+
