ab2518f198114bffcb0ceee8824058c1a51a966c
Dimokus <Dimokus@mail.ru>
2022-04-27 22:55:29 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,9 +5,9 @@ echo 'export STAKE='${STAKE} >> $HOME/.bashrc
 echo 'export MONIKER='${MONIKER} >> $HOME/.bashrc
 echo 'export LINK_ARWEAVE_WALLET='${LINK_ARWEAVE_WALLET} >> $HOME/.bashrc
 echo 'export LINK_KYVE_VERSION='${LINK_KYVE_VERSION} >> $HOME/.bashrc
-POOL=0
-source $HOME/.bashrc
 
+source $HOME/.bashrc
+POOL=0
 
 wget -O arweave.json ${LINK_ARWEAVE_WALLET} 
 mv arweave.json /root/arweave.json
@@ -23,19 +23,29 @@ kyve --version
 for ((;;))
 do
 $(which kyve) --name "${MONIKER}" --poolId ${POOL} --mnemonic "${MNEMONIC}" -s "${STAKE}" --keyfile /root/arweave.json --network korellia --verbose
-sleep 5m
-if [[ ${POOL} -gt 0 ]]
-then
-	POOL=0
-	echo ======================================
-	echo ="ВНИМАНИЕ, СМЕНА ПУЛА! Выбран пул 0"=
-	echo ======================================
-else 
+	echo ======================================================================================
+	echo ="Недостаточно средств для попадания в валидаторы, смена пула произойдет через 5 минут"=
+	echo ======================================================================================
+sleep 10
+if [[ $POOL = 0 ]] 
+then	
 	POOL=1
-	echo ======================================
-	echo ="ВНИМАНИЕ, СМЕНА ПУЛА! Выбран пул 1"=
-	echo ======================================
+	echo ===========================================================
+	echo ="ВНИМАНИЕ, СМЕНА ПУЛА! Выбран пул 1 (Avalanche // C-Chain)"=
+	echo ===========================================================
+elif [[ $POOL = 1 ]] 
+then
+	POOL=3
+	echo ===============================================
+	echo ="ВНИМАНИЕ, СМЕНА ПУЛА! Выбран пул 3 (Bitcoin)"=
+	echo ===============================================
+else  
+	POOL=0
+	echo ===============================================
+	echo ="ВНИМАНИЕ, СМЕНА ПУЛА! Выбран пул 0 (Moonbeam)"=
+	echo ===============================================
 fi
+
 done
 
 
