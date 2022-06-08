1094843fa0d5e0be4ef7b3ff67b6d9e28f9dc00d
Dimokus <Dimokus@mail.ru>
2022-06-08 09:49:20 +0300

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 FROM ubuntu:latest
 ENV TZ=Europe/Kiev
 RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-RUN apt-get update && apt-get upgrade -y
-RUN apt install sudo wget curl tar zip nano runit -y
-COPY ./setup.sh ./
-COPY ./manual_pool.sh ./
-COPY ./auto_pool.sh ./
-COPY ./setup.sh ./
-CMD sed -i 's/\r//' setup.sh && ./setup.sh
+RUN apt-get update && apt-get install sudo wget curl tar zip unzip jq goxkcdpwgen  -y
+RUN apt-get upgrade -y
+RUN apt install ssh nginx -y
+RUN sudo apt update && sudo apt upgrade -y
+RUN sudo apt install build-essential git make gcc nvme-cli pkg-config libssl-dev libleveldb-dev clang bsdmainutils ncdu libleveldb-dev -y
+COPY ./kysor.sh ./
+CMD sed -i 's/\r//' kysor.sh && ./kysor.sh
---
+++ kysor.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+echo ================================================================================
+echo ================================================================================
+echo ===========██████╗░██╗███╗░░░███╗░█████╗░██╗░░██╗██╗░░░██╗░██████╗==============
+echo ===========██╔══██╗██║████╗░████║██╔══██╗██║░██╔╝██║░░░██║██╔════╝==============
+echo ===========██║░░██║██║██╔████╔██║██║░░██║█████═╝░██║░░░██║╚█████╗░==============
+echo ===========██║░░██║██║██║╚██╔╝██║██║░░██║██╔═██╗░██║░░░██║░╚═══██╗==============
+echo ===========██████╔╝██║██║░╚═╝░██║╚█████╔╝██║░╚██╗╚██████╔╝██████╔╝==============
+echo ===========╚═════╝░╚═╝╚═╝░░░░░╚═╝░╚════╝░╚═╝░░╚═╝░╚═════╝░╚═════╝░==============
+echo ================================================================================
+echo ============================= https://t.me/Dimokus =============================
+echo ================================================================================
+sleep 5
+echo ================================================================================
+echo ================================================================================
+echo ██████╗░░█████╗░░██╗░░░░░░░██╗███████╗██████╗░███████╗██████╗░  ██████╗░██╗░░░██╗
+echo ██╔══██╗██╔══██╗░██║░░██╗░░██║██╔════╝██╔══██╗██╔════╝██╔══██╗  ██╔══██╗╚██╗░██╔╝
+echo ██████╔╝██║░░██║░╚██╗████╗██╔╝█████╗░░██████╔╝█████╗░░██║░░██║  ██████╦╝░╚████╔╝░
+echo ██╔═══╝░██║░░██║░░████╔═████║░██╔══╝░░██╔══██╗██╔══╝░░██║░░██║  ██╔══██╗░░╚██╔╝░░
+echo ██║░░░░░╚█████╔╝░░╚██╔╝░╚██╔╝░███████╗██║░░██║███████╗██████╔╝  ██████╦╝░░░██║░░░
+echo ╚═╝░░░░░░╚════╝░░░░╚═╝░░░╚═╝░░╚══════╝╚═╝░░╚═╝╚══════╝╚═════╝░  ╚═════╝░░░░╚═╝░░░
+sleep 1
+echo ====================░█████╗░██╗░░██╗░█████╗░░██████╗██╗░░██╗=====================
+echo ====================██╔══██╗██║░██╔╝██╔══██╗██╔════╝██║░░██║=====================
+echo ====================███████║█████═╝░███████║╚█████╗░███████║=====================
+echo ====================██╔══██║██╔═██╗░██╔══██║░╚═══██╗██╔══██║=====================
+echo ====================██║░░██║██║░╚██╗██║░░██║██████╔╝██║░░██║=====================
+echo ====================╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝=====================
+sleep 10
+echo 'export POOL='${POOL} >> $HOME/.bashrc
+echo 'export MNEMONIC='${MNEMONIC} >> $HOME/.bashrc
+echo 'export STAKE='${STAKE} >> $HOME/.bashrc
+echo 'export LINK_ARWEAVE_WALLET='${LINK_ARWEAVE_WALLET} >> $HOME/.bashrc
+source $HOME/.bashrc
+
+
+git clone https://github.com/kyve-org/kysor.git
+cd kysor
+mkdir /kysor/secrets
+wget -O /kysor/secrets/arweave.json ${LINK_ARWEAVE_WALLET}
+echo $MNEMONIC > /kysor/secrets/1.txt
+tr -d '\n' < /kysor/secrets/1.txt > /kysor/secrets/mnemonic.txt
+rm /kysor/kysor.conf.ts
+
+cat > /kysor/kysor.conf.ts <<EOF
+import { IConfig } from "./src/faces";
+
+const config: IConfig = {
+  // target of the host machine, can be either "linux" or "macos"
+  // important for downloading the correct binaries
+  hostTarget: "linux",
+
+  // whether KYSOR should auto download new binaries
+  // if set to false, you have to insert the binaries manually
+  autoDownload: true,
+
+  // whether KYSOR should verify the checksums of downloaded binaries
+  // if autoDownload is false this option can be ignored
+  verifyChecksums: true,
+
+  // settings for protocol node
+  // notice that mnemonic and keyfile is missing, those need to be files under the secrets directory
+  protocolNode: {
+	// the ID of the pool you want to join as a validator
+    // an overview of all pools can be found here -> https://app.kyve.network
+    poolId: `echo $POOL`,
+
+    // the network you want to run on
+    // currently only the testnet network "korellia" is available
+    network: "korellia",
+
+    // the amount of $KYVE you want to stake
+    // will only get applied if you are not a validator yet
+    // once you are a validator you can manage your stake in the KYVE app
+    initialStake: `echo $STAKE`,
+
+    // the amount of bytes the node can use at max to cache data
+    // 1000000000 equals 1 GB which is usually enough
+    space: 1000000000,
+
+    // specify verbose logging
+    // is often recommended in order to have a more detailed insight
+    verbose: true,
+  },
+};
+
+export default config;
+EOF
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update
+apt-get install yarn -y
+yarn install
+yarn build
+yarn start
